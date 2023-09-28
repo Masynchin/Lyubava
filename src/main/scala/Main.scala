55fb106c3d5dfe8f33293971151140b92b8d8fab
Max Smirnov <masynchin@gmail.com>
2023-09-28 20:58:12 +0300

    Rewrite using cats algebra

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeoutException
+
 import cats.implicits.*
 import cats.effect.{IO, IOApp}
 import cats.effect.std.{Console, Random}
@@ -13,24 +15,15 @@ object Lyubava extends IOApp.Simple:
     }
 
   val run =
-    replicate(
-      withFeedback(
-        withTimeout(answer.flatMap(userAttempt), 3.seconds),
-        _.fold("Timeout!")(Function.const("Yahoo!"))
-      )
-    ).void
+    answer
+      .flatMap(userAttempt)
+      .timeoutAndForget(3.seconds)
+      .productR(Console[IO].println("Yahoo!"))
+      .foreverM
+      .handleErrorWith {
+        case _: TimeoutException => Console[IO].println("Timeout!")
+      }
+      .void
 
 def userAttempt(answer: String): IO[String] =
   Console[IO].println(s"Type this: ${answer}") *> Console[IO].readLine
-
-def withTimeout[A](origin: IO[A], duration: Duration): IO[Option[A]] =
-  origin.timeoutAndForget(duration).option
-
-def withFeedback[A](origin: IO[A], show: Show[A]): IO[A] =
-  origin.flatTap(Console[IO].println(_)(using show))
-
-def replicate[A](origin: IO[Option[A]]): IO[List[A]] =
-  origin.flatMap {
-    case Some(a) => replicate(origin).map(a :: _)
-    case None    => IO.pure(Nil)
-  }
